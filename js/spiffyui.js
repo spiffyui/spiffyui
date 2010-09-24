e83d2d5df75344e58cea3486a8c63dd59b6f735b
zgrossbart <zgrossbart@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2010-09-24 21:35:28 +0000

    Added our HTML elements and some more styles.

--- js/spiffyui.js
+++ js/spiffyui.js
@@ -3,4 +3,35 @@
  */
  
 spiffyui = {
+    init: function() {
+        /*
+         * We start by adding the HTML tags our CSS depends on
+         */
+        jQuery(body).append(
+            // We add a message if the user doesn't have JavaScript turned on
+            '<noscript>' + 
+                '<div id="jswarning">' +
+                    'Your browser does not support JavaScript.  You must enable JavaScript ' + 
+                    'and refresh this page to use this tool.' + 
+                '</div>' + 
+            '</noscript>' + 
+            
+            '<div id="loginPanel"></div>' + 
+            '<div id="mainWrap">' + 
+                '<div id="main" style="display:none;" class="clearfix">' + 
+                    '<div id="mainHeader"></div>' + 
+                    '<div id="mainBody">' + 
+                        '<div id="mainNavigation"></div>' + 
+                        '<div id="mainContent"></div>' + 
+                        '<div class="clear"></div>' + 
+                    '</div>' + 
+                    '<div class="clear"></div>' + 
+                '</div>' + 
+            '</div>' + 
+            '<div id="mainFooter"></div>');
+    }
 };
+
+jQuery(document).ready(function() {
+    spiffyui.init();
+});
