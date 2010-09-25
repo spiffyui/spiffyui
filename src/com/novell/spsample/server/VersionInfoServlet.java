b260971f4241ea667d3624c7cef3e2b26183f40f
zgrossbart <zgrossbart@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2010-09-25 10:53:43 +0000

    Added a version info servlet.

---
+++ src/com/novell/spsample/server/VersionInfoServlet.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2010 Unpublished Work of Novell, Inc. All Rights Reserved.
+ *
+ * THIS WORK IS AN UNPUBLISHED WORK AND CONTAINS CONFIDENTIAL,
+ * PROPRIETARY AND TRADE SECRET INFORMATION OF NOVELL, INC. ACCESS TO
+ * THIS WORK IS RESTRICTED TO (I) NOVELL, INC. EMPLOYEES WHO HAVE A NEED
+ * TO KNOW HOW TO PERFORM TASKS WITHIN THE SCOPE OF THEIR ASSIGNMENTS AND
+ * (II) ENTITIES OTHER THAN NOVELL, INC. WHO HAVE ENTERED INTO
+ * APPROPRIATE LICENSE AGREEMENTS. NO PART OF THIS WORK MAY BE USED,
+ * PRACTICED, PERFORMED, COPIED, DISTRIBUTED, REVISED, MODIFIED,
+ * TRANSLATED, ABRIDGED, CONDENSED, EXPANDED, COLLECTED, COMPILED,
+ * LINKED, RECAST, TRANSFORMED OR ADAPTED WITHOUT THE PRIOR WRITTEN
+ * CONSENT OF NOVELL, INC. ANY USE OR EXPLOITATION OF THIS WORK WITHOUT
+ * AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND CIVIL
+ * LIABILITY.
+ *
+ * ========================================================================
+ */
+package com.novell.spssample.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+/**
+ * This servlet shows version infromation.
+ */
+public class BuildInfoServlet extends HttpServlet
+{
+
+    private static final long serialVersionUID = -1l;
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        
+        response.setContentType("application/json");
+        ServletOutputStream out = response.getOutputStream();
+        
+        JSONObject buildinfo = new JSONObject();
+        
+        buildinfo.put("version", "1.0");
+        
+        out.println(buildinfo.toString());
+    }
+}
