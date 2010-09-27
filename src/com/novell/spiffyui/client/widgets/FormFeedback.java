b7dd6c08824ee501e520b01b1a844e1859c8f642
zgrossbart <zgrossbart@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2010-09-27 16:03:51 +0000

    Added the fancy buttons, the form feedback, and a sample form.

---
+++ src/com/novell/spiffyui/client/widgets/FormFeedback.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2010 Unpublished Work of Novell, Inc. All Rights Reserved.
+ *
+ * THIS WORK IS AN UNPUBLISHED WORK AND CONTAINS CONFIDENTIAL,
+ * PROPRIETARY AND TRADE SECRET INFORMATION OF NOVELL, INC. ACCESS TO
+ * THIS WORK IS RESTRICTED TO (I) NOVELL, INC. EMPLOYEES WHO HAVE A NEED
+ * TO KNOW HOW TO PERFORM TASKS WITHIN THE SCOPE OF THEIR ASSIGNMENTS AND
+ * (II) ENTITIES OTHER THAN NOVELL, INC. WHO HAVE ENTERED INTO
+ * APPROPRIATE LICENSE AGREEMENTS. NO PART OF THIS WORK MAY BE USED,
+ * PRACTICED, PERFORMED, COPIED, DISTRIBUTED, REVISED, MODIFIED,
+ * TRANSLATED, ABRIDGED, CONDENSED, EXPANDED, COLLECTED, COMPILED,
+ * LINKED, RECAST, TRANSFORMED OR ADAPTED WITHOUT THE PRIOR WRITTEN
+ * CONSENT OF NOVELL, INC. ANY USE OR EXPLOITATION OF THIS WORK WITHOUT
+ * AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND CIVIL
+ * LIABILITY.
+ *
+ * ========================================================================
+ */
+package com.novell.spiffyui.client.widgets;
+
+import com.google.gwt.user.client.ui.Label;
+
+/**
+ * This widget shows an icon to give feedback on form fields
+ */
+public class FormFeedback extends Label
+{
+    /**
+     * Feedback status warning
+     */
+    public static final int WARNING = 1;
+
+    /**
+     * Feedback status error
+     */
+    public static final int ERROR = 2;
+    
+    /**
+     * Feedback status valid
+     */
+    public static final int VALID = 3;
+    
+    /**
+     * Feedback status loading
+     */
+    public static final int LOADING = 4;
+
+    /**
+     * Feedback status none
+     */
+    public static final int NONE = 5;
+    
+    private static final String WARNING_STYLE = "warning";
+    private static final String ERROR_STYLE = "error";
+    private static final String VALID_STYLE = "valid";
+    private static final String LOADING_STYLE = "loading";
+    
+    private int m_status = NONE;
+    
+    /**
+     * Creates a new FormFeedback widget
+     */
+    public FormFeedback()
+    {
+        this(true);
+    }
+    
+    /**
+     * Creates a new FormFeedback widget
+     * 
+     * @param inform true if this feedback widget needs our standard for styles 
+     *        and false otherwise
+     */
+    public FormFeedback(boolean inform)
+    {
+        getElement().addClassName("spiffy-formfeedback");
+        if (inform) {
+            getElement().addClassName("spiffy-formfeedback-inform");
+        }
+    }
+    
+    private void removeStyles()
+    {
+        getElement().removeClassName(WARNING_STYLE);
+        getElement().removeClassName(ERROR_STYLE);
+        getElement().removeClassName(LOADING_STYLE);
+        getElement().removeClassName(VALID_STYLE);
+    }
+    
+    /**
+     * Sets the status for this FormFeedback
+     * 
+     * @param status the status
+     */
+    public void setStatus(int status)
+    {
+        m_status = status;
+        removeStyles();
+        
+        switch (status) {
+        case WARNING:
+            getElement().addClassName(WARNING_STYLE);
+            return;
+        case ERROR:
+            getElement().addClassName(ERROR_STYLE);
+            return;
+        case LOADING:
+            getElement().addClassName(LOADING_STYLE);
+            return;
+        case VALID:
+            getElement().addClassName(VALID_STYLE);
+            return;
+        default:
+            setTitle("");
+            removeStyles();
+            return;
+        }
+    }
+    
+    /**
+     * Gets the status of this FormFeedback
+     * 
+     * @return the form feedback
+     */
+    public int getStatus()
+    {
+        return m_status;
+    }
+}
---
+++ src/com/novell/spiffyui/client/widgets/button/FancyButton.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2010 Unpublished Work of Novell, Inc. All Rights Reserved.
+ *
+ * THIS WORK IS AN UNPUBLISHED WORK AND CONTAINS CONFIDENTIAL,
+ * PROPRIETARY AND TRADE SECRET INFORMATION OF NOVELL, INC. ACCESS TO
+ * THIS WORK IS RESTRICTED TO (I) NOVELL, INC. EMPLOYEES WHO HAVE A NEED
+ * TO KNOW HOW TO PERFORM TASKS WITHIN THE SCOPE OF THEIR ASSIGNMENTS AND
+ * (II) ENTITIES OTHER THAN NOVELL, INC. WHO HAVE ENTERED INTO
+ * APPROPRIATE LICENSE AGREEMENTS. NO PART OF THIS WORK MAY BE USED,
+ * PRACTICED, PERFORMED, COPIED, DISTRIBUTED, REVISED, MODIFIED,
+ * TRANSLATED, ABRIDGED, CONDENSED, EXPANDED, COLLECTED, COMPILED,
+ * LINKED, RECAST, TRANSFORMED OR ADAPTED WITHOUT THE PRIOR WRITTEN
+ * CONSENT OF NOVELL, INC. ANY USE OR EXPLOITATION OF THIS WORK WITHOUT
+ * AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND CIVIL
+ * LIABILITY.
+ *
+ * ========================================================================
+ */
+package com.novell.spiffyui.client.widgets.button;
+
+import com.google.gwt.user.client.ui.Button;
+
+/**
+ * This is the base class for fancy buttons with or without icons in them
+ */
+public abstract class FancyButton extends Button
+{
+    private boolean m_enabled = true;
+    private boolean m_inProgress = false;
+    
+    /**
+     * Creates a new FancyButton
+     * 
+     * @param text   the text for the button
+     */
+    public FancyButton(String text)
+    {
+        super(text);
+    }
+    
+    @Override
+    public void setEnabled(boolean enabled)
+    {
+        super.setEnabled(enabled);
+        m_enabled = enabled;
+        
+        if (enabled) {
+            getElement().removeClassName("disabled");
+        } else {
+            getElement().addClassName("disabled");
+        }
+    }
+    
+    @Override
+    public boolean isEnabled()
+    {
+        return m_enabled;
+    }
+    
+    /**
+     * Sets this button to be in progress.  In progress buttons show a different
+     * image and are disabled
+     * 
+     * @param inprogress true if in progress and false otherwise
+     */
+    public void setInProgress(boolean inprogress)
+    {
+        m_inProgress = inprogress;
+        setEnabled(!inprogress);
+        
+        if (inprogress) {
+            getElement().addClassName("inprogress");
+        } else {
+            getElement().removeClassName("inprogress");
+        }
+    }
+    
+    /**
+     * Determines if this button is in progress
+     * 
+     * @return true if it is in progress and false otherwise
+     */
+    public boolean isInProgress()
+    {
+        return m_inProgress;
+    }
+}
