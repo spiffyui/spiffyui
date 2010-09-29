b85790bbcce1f612b0e0b8691224dc9c9a93be92
bsiegal <bsiegal@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2010-09-29 00:19:32 +0000

    add SmallLoadingIndicator and RefreshAnchor widgets

---
+++ src/com/novell/spiffyui/client/widgets/SmallLoadingIndicator.java
@@ -0,0 +1,36 @@
+
+/*
+ * Copyright (c) 2010 Unpublished Work of Novell, Inc. All Rights Reserved.
+ *
+ * THIS WORK IS AN UNPUBLISHED WORK AND CONTAINS CONFIDENTIAL,
+ * PROPRIETARY AND TRADE SECRET INFORMATION OF NOVELL, INC. ACCESS TO
+ * THIS WORK IS RESTRICTED TO (I) NOVELL, INC. EMPLOYEES WHO HAVE A NEED
+ * TO KNOW HOW TO PERFORM TASKS WITHIN THE SCOPE OF THEIR ASSIGNMENTS AND
+ * (II) ENTITIES OTHER THAN NOVELL, INC. WHO HAVE ENTERED INTO
+ * APPROPRIATE LICENSE AGREEMENTS. NO PART OF THIS WORK MAY BE USED,
+ * PRACTICED, PERFORMED, COPIED, DISTRIBUTED, REVISED, MODIFIED,
+ * TRANSLATED, ABRIDGED, CONDENSED, EXPANDED, COLLECTED, COMPILED,
+ * LINKED, RECAST, TRANSFORMED OR ADAPTED WITHOUT THE PRIOR WRITTEN
+ * CONSENT OF NOVELL, INC. ANY USE OR EXPLOITATION OF THIS WORK WITHOUT
+ * AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND CIVIL
+ * LIABILITY.
+ *
+ * ========================================================================
+ */
+package com.novell.spiffyui.client.widgets;
+
+import com.google.gwt.user.client.ui.FlowPanel;
+
+/**
+ * This is a reusable loading icon widget
+ *
+ */
+public class SmallLoadingIndicator extends FlowPanel {
+    /**
+     * Creates a new SmallLoadingIndicator
+     */
+    public SmallLoadingIndicator()
+    {
+        addStyleName("loadingSmall");
+    }
+}
---
+++ src/com/novell/spiffyui/client/widgets/button/RefreshAnchor.java
@@ -0,0 +1,102 @@
+/*
+ * ========================================================================
+ *
+ * Copyright (c) 2010 Unpublished Work of Novell, Inc. All Rights Reserved.
+ * 
+ * THIS WORK IS AN UNPUBLISHED WORK AND CONTAINS CONFIDENTIAL,
+ * PROPRIETARY AND TRADE SECRET INFORMATION OF NOVELL, INC. ACCESS TO
+ * THIS WORK IS RESTRICTED TO (I) NOVELL, INC. EMPLOYEES WHO HAVE A NEED
+ * TO KNOW HOW TO PERFORM TASKS WITHIN THE SCOPE OF THEIR ASSIGNMENTS AND
+ * (II) ENTITIES OTHER THAN NOVELL, INC. WHO HAVE ENTERED INTO
+ * APPROPRIATE LICENSE AGREEMENTS. NO PART OF THIS WORK MAY BE USED,
+ * PRACTICED, PERFORMED, COPIED, DISTRIBUTED, REVISED, MODIFIED,
+ * TRANSLATED, ABRIDGED, CONDENSED, EXPANDED, COLLECTED, COMPILED,
+ * LINKED, RECAST, TRANSFORMED OR ADAPTED WITHOUT THE PRIOR WRITTEN
+ * CONSENT OF NOVELL, INC. ANY USE OR EXPLOITATION OF THIS WORK WITHOUT
+ * AUTHORIZATION COULD SUBJECT THE PERPETRATOR TO CRIMINAL AND CIVIL
+ * LIABILITY.
+ *
+ * ========================================================================
+ */
+package com.novell.spiffyui.client.widgets.button;
+
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.Anchor;
+import com.novell.idm.rpt.client.client.IGwtUIStringsRsrc;
+import com.novell.idm.rpt.client.client.Index;
+
+/**
+ * This an Anchor that shows the refresh icon.  It has a disabled look as well as a loading look.
+ */
+public class RefreshAnchor extends Anchor
+{
+    private boolean m_enabled = true;
+    private boolean m_loading = false;
+    private Element m_iconDiv;
+    
+    /**
+     * Creates a new RefreshAnchor
+     * 
+     * @param id - The element ID
+     */
+    public RefreshAnchor(String id)
+    {
+        super();
+        m_iconDiv = Document.get().createDivElement();
+        m_iconDiv.setClassName("rpt-refresh-icon");
+        m_iconDiv.setId(id + "_refreshIconDiv");     
+        
+        getElement().appendChild(m_iconDiv);
+        setHref("#");
+        getElement().setId(id);
+        setTitle(Index.STRINGS.get(IGwtUIStringsRsrc.REFRESH));
+    }
+    
+    @Override
+    public void setEnabled(boolean enabled)
+    {
+        super.setEnabled(enabled);
+        m_enabled = enabled;
+        
+        if (enabled) {
+            m_iconDiv.setClassName("rpt-refresh-icon");
+        } else {
+            m_iconDiv.setClassName("rpt-refresh-icon-disabled");
+        }
+    }
+    
+    
+    @Override
+    public boolean isEnabled()
+    {
+        return m_enabled;
+    }
+    
+
+    /**
+     * Sets the anchor to appear as loading
+     * @param loading - boolean true to be loading
+     */
+    public void setLoading(boolean loading)
+    {
+        super.setEnabled(!loading);
+        m_loading = loading;
+        
+        if (loading) {
+            m_iconDiv.setClassName("rpt-refresh-icon-loading");
+        } else {
+            m_iconDiv.setClassName("rpt-refresh-icon");
+        }
+    }
+
+    /**
+     * Returns true if loading
+     * @return boolean true if loading
+     */
+    public boolean isLoading()
+    {
+        return m_loading;
+    }
+    
+}
