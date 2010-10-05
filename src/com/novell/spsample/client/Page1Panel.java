c21c7beec51c15f2422fc8c3d8c8c99823732fde
zgrossbart <zgrossbart@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2010-10-05 21:24:43 +0000

    A little line editing.

--- src/com/novell/spsample/client/Page1Panel.java
+++ src/com/novell/spsample/client/Page1Panel.java
@@ -85,8 +85,9 @@ public class Page1Panel extends HTMLPanel implements CloseHandler<PopupPanel>
                  "<div class=\"cell tallcell weak\">" +
                     "<h3>Humanized Messages</h3>" + 
                     "The Spiffy UI framework support has an integrated message framework following the pattern of humanized " + 
-                    "messages.  These messages are non-modal and fade away on their own.  They include info messages, warnings, " + 
-                    "errors, and fatal errors.  Errors and some warnings are sent to an error log at the bottom of the screen.<br /><br />" + 
+                    "messages.  These messages are non-modal and fade away without requiring further interaction.  They " + 
+                    "include info messages, warnings, errors, and fatal errors.  Errors and some warnings are sent to an error " + 
+                    "log at the bottom of the screen.<br /><br />" + 
                     "<span id=\"Page1Messages\"></span>" +
                  "</div>" +
 
