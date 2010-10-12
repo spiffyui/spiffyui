117ec3341e1290565f213f78cb56dda3d80095ec
zgrossbart <zgrossbart@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2010-10-12 15:44:59 +0000

    Added a new panel for security information.

--- src/com/novell/spsample/client/AuthPanel.java
+++ src/com/novell/spsample/client/AuthPanel.java
@@ -18,18 +18,29 @@
  */
 package com.novell.spsample.client;
 
-import com.google.gwt.i18n.client.Messages;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
- * Localized strings for the library classes
+ * This is the authentication documentation panel
  *
  */
-public interface SPSampleStrings extends Messages {
-
-    public String AuthPanel_html();
-    public String BuildPanel_html();
-    public String CSSPanel_html();
-    public String DocPanel_html();
-    public String OverviewPanel_html();
-
+public class AuthPanel extends HTMLPanel
+{
+    private static final SPSampleStrings STRINGS = (SPSampleStrings) GWT.create(SPSampleStrings.class);
+    
+	/**
+     * Creates a new panel
+     */
+    public AuthPanel()
+    {
+        super("div", STRINGS.AuthPanel_html());
+        
+        getElement().setId("authPanel");
+        
+        RootPanel.get("mainContent").add(this);
+        
+        setVisible(false);
+    }
 }
--- src/com/novell/spsample/client/Index.java
+++ src/com/novell/spsample/client/Index.java
@@ -122,6 +122,14 @@ public class Index implements EntryPoint, NavBarListener, RESTLoginCallBack
                            "This is the navigation item for the spiffy build page");
         m_navBar.add(item);
         m_panels.put(item, new BuildPanel());
+        
+        /*
+         The authentication info panel
+         */
+        item = new NavItem("authNavItem", "Spiffy Security",
+                           "This is the navigation item for the spiffy authentication page");
+        m_navBar.add(item);
+        m_panels.put(item, new AuthPanel());
 
         /*
          A separator
