fe7eba24444e87adadb0eaec287fca777dc72389
zgrossbart <zgrossbart@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2010-10-19 13:34:49 +0000

    Expanded for spiffy form example.

--- src/com/novell/spsample/client/FormPanel.java
+++ src/com/novell/spsample/client/FormPanel.java
@@ -26,7 +26,9 @@ import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.novell.spiffyui.client.MessageUtil;
 import com.novell.spiffyui.client.widgets.FormFeedback;
@@ -43,26 +45,41 @@ public class FormPanel extends HTMLPanel implements KeyUpHandler
     
     private static final String CONTENTS =
         "<fieldset id=\"Page2Fields\">" +
-            "<h2 class=\"sectionTitle\">Sample form field set</h2>" +
+            "<h2 class=\"sectionTitle\">Sample user form fieldset</h2>" +
             "<ol class=\"dialogformsection\">" +
-                "<li id=\"firstValueRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"firstValueTxt\">Value One: " + 
-                    "</label><div id=\"firstValue\" class=\"formcontrolssection\"></div></li>" +
-                "<li id=\"secondValueRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"secondValueTxt\">Value Two: " + 
-                    "</label><div id=\"secondValue\" class=\"formcontrolssection\"></div></li>" +
-                "<li id=\"thirdValueRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"thirdValueTxt\">Value Three: " + 
-                    "</label><div id=\"thirdValue\" class=\"formcontrolssection\"></div></li>" +
+                "<li id=\"firstNameRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"firstNameTxt\">First name: " + 
+                    "</label><div id=\"firstName\" class=\"formcontrolssection\"></div></li>" +
+                "<li id=\"lastNameRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"lastNameTxt\">Last name: " + 
+                    "</label><div id=\"lastName\" class=\"formcontrolssection\"></div></li>" +
+                "<li id=\"emailRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"emailTxt\">Email address: " + 
+                    "</label><div id=\"email\" class=\"formcontrolssection\"></div></li>" +
+                "<li id=\"passwordRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"passwordTxt\">Password: " + 
+                    "</label><div id=\"password\" class=\"formcontrolssection\"></div></li>" +
+                "<li id=\"passwordRepeatRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"passwordRepeatTxt\">Repeat password: " + 
+                    "</label><div id=\"passwordRepeat\" class=\"formcontrolssection\"></div></li>" +
+                "<li id=\"userDescRow\" class=\"extratallformrow\"><label class=\"dialogformlabel\" for=\"userDescTxt\">Short description: " + 
+                    "</label><div id=\"userDesc\" class=\"formcontrolssection\"></div></li>" +
                 "<li id=\"page2ButtonsRow\" class=\"dialogformrow\"><div id=\"page2Buttons\" class=\"formcontrolssection\"></div></li>" +
             "</ol>" +
         "</fieldset>";
     
-    private TextBox m_field1;
-    private FormFeedback m_field1Feedback;
+    private TextBox m_firstName;
+    private FormFeedback m_firstNameFeedback;
     
-    private TextBox m_field2;
-    private FormFeedback m_field2Feedback;
+    private TextBox m_lastName;
+    private FormFeedback m_lastNameFeedback;
     
-    private TextBox m_field3;
-    private FormFeedback m_field3Feedback;
+    private TextBox m_email;
+    private FormFeedback m_emailFeedback;
+    
+    private TextBox m_password;
+    private FormFeedback m_passwordFeedback;
+    
+    private TextBox m_passwordRepeat;
+    private FormFeedback m_passwordRepeatFeedback;
+    
+    private TextArea m_userDesc;
+    private FormFeedback m_userDescFeedback;
     
     private FancyButton m_save;
     
@@ -80,40 +97,76 @@ public class FormPanel extends HTMLPanel implements KeyUpHandler
         setVisible(false);
         
         /*
-         Field 1
+         First name
          */
-        m_field1 = new TextBox();
-        m_field1.addKeyUpHandler(this);
-        m_field1.getElement().setId("firstValueTxt");
-        m_field1.getElement().addClassName("wideTextField");
-        add(m_field1, "firstValue");
+        m_firstName = new TextBox();
+        m_firstName.addKeyUpHandler(this);
+        m_firstName.getElement().setId("firstNameTxt");
+        m_firstName.getElement().addClassName("wideTextField");
+        add(m_firstName, "firstName");
         
-        m_field1Feedback = new FormFeedback();
-        add(m_field1Feedback, "firstValueRow");
+        m_firstNameFeedback = new FormFeedback();
+        add(m_firstNameFeedback, "firstNameRow");
         
         /*
-         Field 2
+         Last name
          */
-        m_field2 = new TextBox();
-        m_field2.addKeyUpHandler(this);
-        m_field2.getElement().setId("secondValueTxt");
-        m_field2.getElement().addClassName("wideTextField");
-        add(m_field2, "secondValue");
+        m_lastName = new TextBox();
+        m_lastName.addKeyUpHandler(this);
+        m_lastName.getElement().setId("lastNameTxt");
+        m_lastName.getElement().addClassName("wideTextField");
+        add(m_lastName, "lastName");
         
-        m_field2Feedback = new FormFeedback();
-        add(m_field2Feedback, "secondValueRow");
+        m_lastNameFeedback = new FormFeedback();
+        add(m_lastNameFeedback, "lastNameRow");
         
         /*
-         Field 3
+         email
          */
-        m_field3 = new TextBox();
-        m_field3.addKeyUpHandler(this);
-        m_field3.getElement().setId("thirdValueTxt");
-        m_field3.getElement().addClassName("wideTextField");
-        add(m_field3, "thirdValue");
+        m_email = new TextBox();
+        m_email.addKeyUpHandler(this);
+        m_email.getElement().setId("emailTxt");
+        m_email.getElement().addClassName("wideTextField");
+        add(m_email, "email");
         
-        m_field3Feedback = new FormFeedback();
-        add(m_field3Feedback, "thirdValueRow");
+        m_emailFeedback = new FormFeedback();
+        add(m_emailFeedback, "emailRow");
+        
+        /*
+         Password
+         */
+        m_password = new PasswordTextBox();
+        m_password.addKeyUpHandler(this);
+        m_password.getElement().setId("passwordTxt");
+        m_password.getElement().addClassName("slimTextField");
+        add(m_password, "password");
+        
+        m_passwordFeedback = new FormFeedback();
+        add(m_passwordFeedback, "passwordRow");
+        
+        /*
+         Password repeat
+         */
+        m_passwordRepeat = new PasswordTextBox();
+        m_passwordRepeat.addKeyUpHandler(this);
+        m_passwordRepeat.getElement().setId("passwordRepeatTxt");
+        m_passwordRepeat.getElement().addClassName("slimTextField");
+        add(m_passwordRepeat, "passwordRepeat");
+        
+        m_passwordRepeatFeedback = new FormFeedback();
+        add(m_passwordRepeatFeedback, "passwordRepeatRow");
+        
+        /*
+         User description
+         */
+        m_userDesc = new TextArea();
+        m_userDesc.addKeyUpHandler(this);
+        m_userDesc.getElement().setId("userDescTxt");
+        m_userDesc.getElement().addClassName("wideTextField");
+        add(m_userDesc, "userDesc");
+        
+        m_userDescFeedback = new FormFeedback();
+        add(m_userDescFeedback, "userDescRow");
         
         /*
          The big save button
@@ -140,30 +193,60 @@ public class FormPanel extends HTMLPanel implements KeyUpHandler
     
     private void updateFormStatus(Widget w)
     {
-        if (w == m_field1) {
-            if (m_field1.getText().length() > 2) {
-                m_field1Feedback.setStatus(FormFeedback.VALID);
+        if (w == m_firstName) {
+            if (m_firstName.getText().length() > 2) {
+                m_firstNameFeedback.setStatus(FormFeedback.VALID);
             } else {
-                m_field1Feedback.setStatus(FormFeedback.WARNING);
+                m_firstNameFeedback.setStatus(FormFeedback.WARNING);
             }
-        } else if (w == m_field2) {
-            if (m_field2.getText().length() > 2) {
-                m_field2Feedback.setStatus(FormFeedback.VALID);
+        } else if (w == m_lastName) {
+            if (m_lastName.getText().length() > 2) {
+                m_lastNameFeedback.setStatus(FormFeedback.VALID);
             } else {
-                m_field2Feedback.setStatus(FormFeedback.WARNING);
+                m_lastNameFeedback.setStatus(FormFeedback.WARNING);
             }
-        } else if (w == m_field3) {
-            if (m_field3.getText().length() > 2) {
-                m_field3Feedback.setStatus(FormFeedback.VALID);
+        } else if (w == m_email) {
+            if (m_email.getText().length() > 2) {
+                m_emailFeedback.setStatus(FormFeedback.VALID);
             } else {
-                m_field3Feedback.setStatus(FormFeedback.WARNING);
+                m_emailFeedback.setStatus(FormFeedback.WARNING);
             }
-        }
+        } else if (w == m_password) {
+            if (m_password.getText().length() > 2) {
+                m_passwordFeedback.setStatus(FormFeedback.VALID);
+            } else {
+                m_passwordFeedback.setStatus(FormFeedback.WARNING);
+            }
+        } else if (w == m_passwordRepeat) {
+            if (m_passwordRepeat.getText().length() > 2) {
+                if (m_passwordRepeat.getText().equals(m_password.getText())) {
+                    m_passwordRepeatFeedback.setStatus(FormFeedback.VALID);
+                    m_passwordRepeatFeedback.setTitle("");
+                } else {
+                    m_passwordRepeatFeedback.setStatus(FormFeedback.ERROR);
+                    m_passwordRepeatFeedback.setTitle("Make sure your two passwords match.");
+                }
+            } else {
+                m_passwordRepeatFeedback.setStatus(FormFeedback.WARNING);
+                m_passwordRepeatFeedback.setTitle("");
+            }
+        } else if (w == m_userDesc) {
+            if (m_userDesc.getText().length() > 8) {
+                m_userDescFeedback.setStatus(FormFeedback.VALID);
+            } else {
+                m_userDescFeedback.setStatus(FormFeedback.WARNING);
+            }
+        } 
+
 
         
-        m_save.setEnabled(m_field1.getText().length() > 2 &&
-                          m_field2.getText().length() > 2 &&
-                          m_field3.getText().length() > 2);
+        m_save.setEnabled(m_firstName.getText().length() > 2 &&
+                          m_lastName.getText().length() > 2 &&
+                          m_email.getText().length() > 2 &&
+                          m_password.getText().length() > 2 &&
+                          m_passwordRepeat.getText().length() > 2 &&
+                          m_passwordRepeat.getText().equals(m_password.getText()) &&
+                          m_userDesc.getText().length() > 8);
     }
     
     private void save()
