0e25879917f84b3c46c8a0aec3588f0fefc2f00b
zgrossbart <zgrossbart@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2010-10-22 11:26:25 +0000

    Added some more sample form controls and fixed a small error in the login image.

--- src/com/novell/spsample/client/FormPanel.java
+++ src/com/novell/spsample/client/FormPanel.java
@@ -53,12 +53,18 @@ public class FormPanel extends HTMLPanel implements KeyUpHandler
                     "</label><div id=\"lastName\" class=\"formcontrolssection\"></div></li>" +
                 "<li id=\"emailRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"emailTxt\">Email address: " + 
                     "</label><div id=\"email\" class=\"formcontrolssection\"></div></li>" +
+                "<li id=\"userDescRow\" class=\"extratallformrow\"><label class=\"dialogformlabel\" for=\"userDescTxt\">Short description: " + 
+                    "</label><div id=\"userDesc\" class=\"formcontrolssection\"></div></li>" +
+                "<h3 class=\"subSectionTitle\">Passwords</h3>" +
                 "<li id=\"passwordRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"passwordTxt\">Password: " + 
                     "</label><div id=\"password\" class=\"formcontrolssection\"></div></li>" +
                 "<li id=\"passwordRepeatRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"passwordRepeatTxt\">Repeat password: " + 
                     "</label><div id=\"passwordRepeat\" class=\"formcontrolssection\"></div></li>" +
-                "<li id=\"userDescRow\" class=\"extratallformrow\"><label class=\"dialogformlabel\" for=\"userDescTxt\">Short description: " + 
-                    "</label><div id=\"userDesc\" class=\"formcontrolssection\"></div></li>" +
+                "<li id=\"securityQuestionRow\" class=\"extratallformrow\"><label class=\"dialogformlabel\" for=\"securityQuestionTxt\">Security question: " + 
+                    "</label><div id=\"securityQuestion\" class=\"formcontrolssection\"></div>" + 
+                "</li>" +
+                "<li id=\"securityAnswerRow\" class=\"dialogformrow\"><label class=\"dialogformlabel\" for=\"securityAnswerTxt\">Security Answer: " + 
+                    "</label><div id=\"securityAnswer\" class=\"formcontrolssection\"></div></li>" +
                 "<li id=\"page2ButtonsRow\" class=\"dialogformrow\"><div id=\"page2Buttons\" class=\"formcontrolssection\"></div></li>" +
             "</ol>" +
         "</fieldset>";
@@ -80,6 +86,12 @@ public class FormPanel extends HTMLPanel implements KeyUpHandler
     
     private TextArea m_userDesc;
     private FormFeedback m_userDescFeedback;
+    
+    private TextArea m_securityQuestion;
+    private FormFeedback m_securityQuestionFeedback;
+    
+    private TextBox m_securityAnswer;
+    private FormFeedback m_securityAnswerFeedback;
     
     private FancyButton m_save;
     
@@ -133,6 +145,18 @@ public class FormPanel extends HTMLPanel implements KeyUpHandler
         add(m_emailFeedback, "emailRow");
         
         /*
+         User description
+         */
+        m_userDesc = new TextArea();
+        m_userDesc.addKeyUpHandler(this);
+        m_userDesc.getElement().setId("userDescTxt");
+        m_userDesc.getElement().addClassName("wideTextField");
+        add(m_userDesc, "userDesc");
+        
+        m_userDescFeedback = new FormFeedback();
+        add(m_userDescFeedback, "userDescRow");
+        
+        /*
          Password
          */
         m_password = new PasswordTextBox();
@@ -157,16 +181,30 @@ public class FormPanel extends HTMLPanel implements KeyUpHandler
         add(m_passwordRepeatFeedback, "passwordRepeatRow");
         
         /*
-         User description
+         Security Question
          */
-        m_userDesc = new TextArea();
-        m_userDesc.addKeyUpHandler(this);
-        m_userDesc.getElement().setId("userDescTxt");
-        m_userDesc.getElement().addClassName("wideTextField");
-        add(m_userDesc, "userDesc");
+        m_securityQuestion = new TextArea();
+        m_securityQuestion.addKeyUpHandler(this);
+        m_securityQuestion.getElement().setId("securityQuestionTxt");
+        m_securityQuestion.getElement().addClassName("wideTextField");
+        add(m_securityQuestion, "securityQuestion");
         
-        m_userDescFeedback = new FormFeedback();
-        add(m_userDescFeedback, "userDescRow");
+        m_securityQuestionFeedback = new FormFeedback();
+        add(m_securityQuestionFeedback, "securityQuestionRow");
+        
+        /*
+         Security answer
+         */
+        m_securityAnswer = new TextBox();
+        m_securityAnswer.addKeyUpHandler(this);
+        m_securityAnswer.getElement().setId("securityAnswerTxt");
+        m_securityAnswer.getElement().addClassName("wideTextField");
+        add(m_securityAnswer, "securityAnswer");
+        
+        m_securityAnswerFeedback = new FormFeedback();
+        add(m_securityAnswerFeedback, "securityAnswerRow");
+        
+        
         
         /*
          The big save button
@@ -216,6 +254,18 @@ public class FormPanel extends HTMLPanel implements KeyUpHandler
                 m_passwordFeedback.setStatus(FormFeedback.VALID);
             } else {
                 m_passwordFeedback.setStatus(FormFeedback.WARNING);
+            }
+        } else if (w == m_securityQuestion) {
+            if (m_securityQuestion.getText().length() > 2) {
+                m_securityQuestionFeedback.setStatus(FormFeedback.VALID);
+            } else {
+                m_securityQuestionFeedback.setStatus(FormFeedback.WARNING);
+            }
+        } else if (w == m_securityAnswer) {
+            if (m_securityAnswer.getText().length() > 2) {
+                m_securityAnswerFeedback.setStatus(FormFeedback.VALID);
+            } else {
+                m_securityAnswerFeedback.setStatus(FormFeedback.WARNING);
             }
         } else if (w == m_passwordRepeat) {
             if (m_passwordRepeat.getText().length() > 2) {
