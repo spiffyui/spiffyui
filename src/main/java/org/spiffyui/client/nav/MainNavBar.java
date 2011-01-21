cc05fe5b50fccac33b4b88ee2237e495b0977c2b
lwashington <lwashington@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2011-01-21 23:43:26 +0000

    Updated to allow subclasses to be able to alter the type of history token that is stored.  Also a change to allow a selection to occur and suppress the firing of events to notify listeners of the selection change.

--- src/main/java/org/spiffyui/client/nav/MainNavBar.java
+++ src/main/java/org/spiffyui/client/nav/MainNavBar.java
@@ -21,40 +21,40 @@ package org.spiffyui.client.nav;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.spiffyui.client.HistoryCallback;
+import org.spiffyui.client.JSUtil;
+
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 
-import org.spiffyui.client.JSUtil;
-import org.spiffyui.client.HistoryCallback;
-
 /**
  * This is the navigation bar for the main page.
  *
  */
 public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler, HistoryCallback
-{    
-    private List<NavItem> m_items = new ArrayList<NavItem>();
+{
+    private final List<NavItem> m_items = new ArrayList<NavItem>();
     private NavItem m_selectedItem = null;
-    
+
     /**
      * The class used for selected navigation items
      */
     protected static final String SELECTED_CLASS = "navItemSelected";
-     
-    
+
+
     /**
      * Create a new MainNavBar
      */
     public MainNavBar()
     {
         getElement().setId("mainNavContainer");
-        
+
         RootPanel.get("mainNavigation").add(this);
     }
-    
+
     @Override
     public void add(Widget w)
     {
@@ -63,7 +63,7 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
             (w instanceof NavHeader) ||
             (w instanceof NavSeparator)) {
             super.add(w);
-            
+
             if (w instanceof NavItem) {
                 m_items.add((NavItem) w);
                 ((NavItem) w).getAnchor().addClickHandler(this);
@@ -74,10 +74,10 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
             throw new IllegalArgumentException("You can only add NavItem, NavHeader, NavSection, or NavSeparator to this class");
         }
     }
-    
+
     /**
      * Addes a nav item to the list of anchors to listen to
-     * 
+     *
      * @param item   the nav item to add
      */
     protected void addNavItem(NavItem item)
@@ -87,7 +87,7 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
             m_items.add(item);
         }
     }
-    
+
     @Override
     public void onClick(ClickEvent event)
     {
@@ -95,7 +95,7 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
         if (!isEnabled()) {
             return;
         }
-        
+
         NavItem navItem = null;
         for (Widget w : getChildren()) {
             if (w instanceof NavItem) {
@@ -112,11 +112,22 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
                 }
             }
         }
-        
+
         doFireEvent(event, navItem);
-        
+
     }
 
+    /**
+     * Call-back when the history item is retrieved after a browser back or forward
+     * button is pressed.  The default implementation will be to treat the supplied id
+     * as a {@link NavItem} reference and select the one with the same ID.
+     *
+     * If you subclass and overload the {@link #addHistoryItem(String)} routine, you
+     * will probably need to overload this method as well, to ensure that the supplied id
+     * (or history token) is translated correctly for your subclass.
+     *
+     * @param id The history token previously stored on the history stack.
+     */
     @Override
     public void historyChanged(String id)
     {
@@ -125,7 +136,7 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
             selectItem(item, false, true);
         }
     }
-    
+
     private void doFireEvent(ClickEvent event, NavItem navItem)
     {
         //if any listener wants to cancel the event
@@ -133,7 +144,7 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
         if (!firePreEvent(navItem)) {
             return;
         }
-        
+
         //continue if no listener returned false on the pre-event
         for (Widget w : getChildren()) {
             if (w instanceof NavItem) {
@@ -150,12 +161,12 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
             }
         }
     }
-    
+
     /**
      * Selects the specified navigation item and fires the navigation event to
      * let all listeners know it was selected.  This intentionally does not
      * fire a pre-event so that it cannot be intercepted. Returns true if NavItem found, false otherwise
-     * 
+     *
      * @param item   the item to select
      * @return true if the item is one of the NavItems of the NavBar, false if not a member such as logout
      */
@@ -163,33 +174,55 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
     {
         return selectItem(item, true, false);
     }
-    
+
     /**
      * Selects the specified navigation item and fires the navigation event to
      * let all listeners know it was selected.  Returns true if NavItem found, false otherwise
-     * 
+     *
      * @param item   the item to select
      * @param addToHistory
      *               true if this item should be added to the browser's history and false otherwise
      * @param doFirePreEvent
      *               true to allow interception and cancelling of the event, false to not fire the pre-event
-     * 
+     *
      * @return true if the item is one of the NavItems of the NavBar, false if not a member such as logout
      */
     public boolean selectItem(NavItem item, boolean addToHistory, boolean doFirePreEvent)
+    {
+        return selectItem(item, addToHistory, doFirePreEvent, true);
+    }
+
+    /**
+     * Selects the specified navigation item and can fire the navigation event to
+     * let all listeners know it was selected, if the doFireSelectedEvent parameter is set accordingly.
+     * Returns true if NavItem found, false otherwise
+     *
+     * @param item   the item to select
+     * @param addToHistory
+     *               true if this item should be added to the browser's history and false otherwise
+     * @param doFirePreEvent
+     *               true to allow interception and cancelling of the event, false to not fire the pre-event
+     * @param doFireSelectEvent
+     *               true to fire the selection event to navbar listeners, false to not fire the event
+     *
+     * @return true if the item is one of the NavItems of the NavBar, false if not a member such as logout
+     */
+    public boolean selectItem(NavItem item, boolean addToHistory, boolean doFirePreEvent, boolean doFireSelectEvent)
     {
         //if any listener wants to cancel the event
         //then do not continue
         if (doFirePreEvent && !firePreEvent(item)) {
             return false;
         }
-        
+
         boolean found = false;
         for (NavItem ni : m_items) {
             if (ni == item) {
                 ni.addStyleName(SELECTED_CLASS);
                 m_selectedItem = ni;
-                fireEvent(item, addToHistory);
+                if (doFireSelectEvent) {
+                    fireEvent(item, addToHistory);
+                }
                 found = true;
             } else {
                 ni.removeStyleName(SELECTED_CLASS);
@@ -200,19 +233,19 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
 
     /**
      * Get the currently selected navigation item in this navigation bar.
-     * 
+     *
      * @return the currently selected navigation item or null if no items are selected
      */
-    public NavItem getSelectedItem() 
+    public NavItem getSelectedItem()
     {
         return m_selectedItem;
     }
-    
+
     /**
      * Gets a navigation item from the navigation menu.
-     * 
+     *
      * @param id     The id of the item to get
-     * 
+     *
      * @return the item with that id or null if it doesn't exist
      */
     public NavItem getItem(String id)
@@ -222,21 +255,21 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
                 return item;
             }
         }
-        
+
         return null;
-    }   
-    
+    }
+
     /**
      * Set the navigation bar to be enabled or disabled.  A disabled navigation bar
      * is gray and never fires selection events.
-     * 
+     *
      * @param enabled true for enabled and false for disabled
      */
     @Override
     public void setEnabled(boolean enabled)
     {
         super.setEnabled(enabled);
-        
+
         if (enabled) {
             removeStyleName("disabled");
         } else {
@@ -247,14 +280,36 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
             addStyleName("disabled");
         }
     }
-    
+
     @Override
     public void fireEvent(NavItem item, boolean addToHistory)
     {
         super.fireEvent(item, addToHistory);
-        
+
         if (isEnabled() && addToHistory) {
-            JSUtil.addHistoryItem(this, item.getElement().getId());
+            addHistoryItem(item.getElement().getId());
+        }
+    }
+
+    /**
+     * A routine to store a history token that this Navbar can use when called
+     * in the future when the forward or back button's are called.
+     *
+     * This is available for subclasses to overload if they wish to store a token
+     * that is not based on the NavItem's ID.  For instance the situation where
+     * there are multiple types of identifiers being used to reconstitute a past application
+     * state.
+     *
+     * Please note that if you overload this method you will need to provide an alternative implementation
+     * of the history callback routine {@link #historyChanged(String)}. That will need to translate the
+     * stored token that your overloaded addHistoryItem routine stored.
+     *
+     * @param historyToken A string value that will server as a token about the applications state
+     */
+    protected void addHistoryItem(String historyToken)
+    {
+        if (historyToken != null && historyToken.length() > 0) {
+            JSUtil.addHistoryItem(this, historyToken);
         }
     }
 
@@ -263,7 +318,7 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
      *
      */
     @Override
-    public void clear() 
+    public void clear()
     {
         super.clear();
         m_items.clear();
@@ -271,7 +326,7 @@ public class MainNavBar extends HasNavBarListenersPanel implements ClickHandler,
 
     /**
      * Remove the specified NavItem from the navigation bar.
-     * 
+     *
      * @param item   the item to remove
      */
     public void remove(NavItem item)
