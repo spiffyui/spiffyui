279810b24efc5043969d19f992fc1a827d1b74d8
ZGrossbart <ZGrossbart@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2011-02-04 22:16:14 +0000

    Bug 669649.  We are now using the built in fade out function instead of doing a custom animation to hide the humanized messages.

--- src/main/js/lib/humanmsg.js
+++ src/main/js/lib/humanmsg.js
@@ -69,7 +69,7 @@ var humanMsg = {
 	displayMsg: function(msg, /*boolean*/doLog) {
 		if (msg == '')
 			return;
-
+        
         humanMsg.activateLog();
 
         clearTimeout(humanMsg.t2);
@@ -104,9 +104,9 @@ var humanMsg = {
         }
 
 		// Watch for mouse & keyboard in .5s
-		humanMsg.t1 = setTimeout("humanMsg.bindEvents()", 3000)
+		humanMsg.t1 = setTimeout(function() {humanMsg.bindEvents();}, 3000);
 		// Remove message after 5s
-		humanMsg.t2 = setTimeout("humanMsg.removeMsg()", 5000)
+		humanMsg.t2 = setTimeout(function() {humanMsg.removeMsg();}, 5000);
 	},
 	
 	log: function(msg) {
@@ -132,7 +132,7 @@ var humanMsg = {
          clearTimeout(humanMsg.t3);
 
          //fadeout the error log after 15s
-         humanMsg.t3 = setTimeout("humanMsg.fadeLog()", 15000)
+         humanMsg.t3 = setTimeout(humanMsg.fadeLog, 15000)
     },
 	
 	setLogHeight: function() {
@@ -169,15 +169,13 @@ var humanMsg = {
 	},
 
 	removeMsg: function() {
-		// Unbind mouse & keyboard
+         // Unbind mouse & keyboard
 		jQuery(window)
 			.unbind('mousemove', humanMsg.removeMsg)
 			.unbind('click', humanMsg.removeMsg)
 			.unbind('keypress', humanMsg.removeMsg)
 
-		// If message is fully transparent, fade it out
-		if (jQuery('#'+humanMsg.msgID).css('opacity') == humanMsg.msgOpacity)
-			jQuery('#'+humanMsg.msgID).animate({ opacity: 0 }, 500, function() { jQuery(this).hide() })
+        jQuery('#'+humanMsg.msgID).fadeOut();
 	},
 
     fadeLog: function() {
