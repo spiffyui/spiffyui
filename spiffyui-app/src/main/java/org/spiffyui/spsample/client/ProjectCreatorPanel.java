91d597e04c9c6a30c7c4f6300c528d78d4cab894
hualinyip@gmail.com <hualinyip@gmail.com@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2011-05-26 21:35:36 +0000

    create project validation: validating project name done, validating java package is still work in progress

--- spiffyui-app/src/main/java/org/spiffyui/spsample/client/ProjectCreatorPanel.java
+++ spiffyui-app/src/main/java/org/spiffyui/spsample/client/ProjectCreatorPanel.java
@@ -15,22 +15,29 @@
  ******************************************************************************/
  package org.spiffyui.spsample.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.spiffyui.client.widgets.FormFeedback;
 import org.spiffyui.client.widgets.button.SimpleButton;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.TextBox;
- 
+import com.google.gwt.user.client.ui.Widget;
+
 
 /**
  * This is the landing panel
  *
  */
-public class ProjectCreatorPanel extends HTMLPanel
+public class ProjectCreatorPanel extends HTMLPanel implements KeyUpHandler
 {
     private static final SPSampleStrings STRINGS = (SPSampleStrings) GWT.create(SPSampleStrings.class);
     
@@ -41,6 +48,8 @@ public class ProjectCreatorPanel extends HTMLPanel
     
     private TextBox m_packageName;
     private FormFeedback m_packageNameFeedback;
+
+    private List<FormFeedback> m_feedbacks = new ArrayList<FormFeedback>();
 
     private SimpleButton m_submit;
 
@@ -55,23 +64,23 @@ public class ProjectCreatorPanel extends HTMLPanel
          * Add project and package fields and button
          */
         m_projectName = new TextBox();
-        //       m_projectName.addKeyUpHandler(this);
+        m_projectName.addKeyUpHandler(this);
         m_projectName.getElement().setId("projectNameTxt");
         m_projectName.getElement().addClassName(WIDE_TEXT_FIELD);
         add(m_projectName, "projectName");
            
         m_projectNameFeedback = new FormFeedback();
-        //       m_feedbacks.add(m_projectNameFeedback);
+        m_feedbacks.add(m_projectNameFeedback);
         add(m_projectNameFeedback, "projectNameRow");
            
         m_packageName = new TextBox();
-        //       m_packageName.addKeyUpHandler(this);
+        m_packageName.addKeyUpHandler(this);
         m_packageName.getElement().setId("packageNameTxt");
         m_packageName.getElement().addClassName(WIDE_TEXT_FIELD);
         add(m_packageName, "packageName");
            
         m_packageNameFeedback = new FormFeedback();
-        //       m_feedbacks.add(m_packageNameFeedback);
+        m_feedbacks.add(m_packageNameFeedback);
         add(m_packageNameFeedback, "packageNameRow");
           
         m_submit = new SimpleButton(Index.getStrings().projectCreatorSubmit());
@@ -83,8 +92,86 @@ public class ProjectCreatorPanel extends HTMLPanel
         });
   
         add(m_submit, "projectBuilderButtons");
-    }    
-    
+        updateFormStatus(null);
+    }
+
+    @Override
+    public void onKeyUp(KeyUpEvent event)
+    {
+        if (event.getNativeKeyCode() != KeyCodes.KEY_TAB) {
+            updateFormStatus((Widget) event.getSource());
+        }
+    }
+
+    private void updateFormStatus(Widget w)
+    {
+        if (w == m_projectName) {
+            validateProjectName();
+        } else if (w == m_packageName) {
+            validatePackageName();
+        }
+        enableCreateButton();
+    }
+
+    /**
+     * Validate that the project name field is filled in and valid
+     * It must contain only alpha numeric characters, and length must be at least 2
+     */
+    private void validateProjectName()
+    {
+        if (m_projectName.getText() != null && m_projectName.getText().length() > 1) {
+            if (isAlphaNumericUnderscore(m_projectName.getText())) {
+                m_projectNameFeedback.setStatus(FormFeedback.VALID);
+                m_projectNameFeedback.setTitle("");
+            } else {
+               m_projectNameFeedback.setStatus(FormFeedback.ERROR);
+               m_projectNameFeedback.setTitle(Index.getStrings().projNameValidChar_tt());
+            }
+        } else {
+            m_projectNameFeedback.setStatus(FormFeedback.WARNING);
+            m_projectNameFeedback.setTitle(Index.getStrings().projNameTooShort_tt());
+        }
+    }
+
+    private boolean isAlphaNumericUnderscore(String str)
+    {
+        for (int i = 0; i < str.length(); i++) {
+            //If we find a non-alphanumeric character we return false.
+            if ((!Character.isLetterOrDigit(str.charAt(i))) && (!(Character.toString(str.charAt(i)).equals("_")))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * Validate that the email field is filled in with a valid email address.
+     */
+    private void validatePackageName()
+    {
+        //todo
+        m_packageNameFeedback.setStatus(FormFeedback.VALID);
+        m_packageNameFeedback.setTitle("");
+    }
+
+    /**
+     * Enable or disable the create project button based on the state of the fields.
+     */
+    private void enableCreateButton()
+    {
+        /*
+         * We only want to enable the create button if every field is valid
+         */
+        for (FormFeedback feedback : m_feedbacks) {
+            if (feedback.getStatus() != FormFeedback.VALID) {
+                m_submit.setEnabled(false);
+                return;
+            }
+        }
+        m_submit.setEnabled(true);
+    }
+
     private void createProject()
     {
         logToGoogleAnalytics();
