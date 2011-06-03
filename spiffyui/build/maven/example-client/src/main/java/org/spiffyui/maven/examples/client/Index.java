5014171c41c3f502155743f4a5c7d2ad19133a3d
gregory.haskins@gmail.com <gregory.haskins@gmail.com@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2011-06-03 15:36:58 +0000

    Initial import of maven-spiffyui-plugin from decoupled branch
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

---
+++ spiffyui/build/maven/example-client/src/main/java/org/spiffyui/maven/examples/client/Index.java
@@ -0,0 +1,142 @@
+package org.spiffyui.maven.examples.client;
+
+import org.spiffyui.client.JSONUtil;
+import org.spiffyui.client.MainHeader;
+import org.spiffyui.client.MessageUtil;
+import org.spiffyui.client.rest.RESTCallback;
+import org.spiffyui.client.rest.RESTException;
+import org.spiffyui.client.rest.RESTility;
+import org.spiffyui.client.widgets.LongMessage;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONValue;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+
+
+/**
+ * This class is the main entry point for our GWT module.
+ */
+public class Index implements EntryPoint, ClickHandler, KeyPressHandler 
+{
+
+    private static Index g_index;
+    private TextBox m_text = new TextBox();
+    private LongMessage m_longMessage = new LongMessage("longMsg");
+
+    /**
+     * The Index page constructor
+     */
+    public Index()
+    {
+        g_index = this;
+    }
+
+
+    @Override
+    public void onModuleLoad()
+    {
+        MainHeader header = new MainHeader();
+        header.setHeaderTitle("Hello Spiffy REST!");
+        
+        FlowPanel panel = new FlowPanel()
+        {
+            @Override
+            public void onLoad()
+            {
+                super.onLoad();
+                /*
+                 Let's set focus into the text field when the page first loads
+                 */
+                m_text.setFocus(true);
+            }
+        };
+        panel.add(m_longMessage);
+        final InlineLabel label = new InlineLabel("What's your name? ");
+        label.setHeight("1em");
+        panel.add(label);
+        panel.add(m_text);
+        final Button button = new Button("Submit");
+        panel.add(button);
+        
+        button.addClickHandler(this);
+        m_text.addKeyPressHandler(this);
+        
+        RootPanel.get("mainContent").add(panel);
+    }
+    
+    @Override
+    public void onClick(ClickEvent event)
+    {
+        sendRequest();
+    }
+
+    @Override
+    public void onKeyPress(KeyPressEvent event)
+    {
+        /*
+         We want to submit the request if the user pressed enter
+         */
+        if (event.getNativeEvent().getKeyCode() == KeyCodes.KEY_ENTER) {
+            sendRequest();
+        }
+    }
+    
+    /**
+     * Send the REST request to the server and read the response back.
+     */
+    private void sendRequest()
+    {
+        String q = m_text.getValue().trim();
+        if (q.equals("")) {
+            MessageUtil.showWarning("Please enter your name in the text field.", false);
+            return;
+        }
+        RESTility.callREST("/api/hello?user=" + q, new RESTCallback() {
+            
+            @Override
+            public void onSuccess(JSONValue val)
+            {
+                showSuccessMessage(val);
+            }
+            
+            @Override
+            public void onError(int statusCode, String errorResponse)
+            {
+                MessageUtil.showError("Error.  Status Code: " + statusCode + " " + errorResponse);
+            }
+            
+            @Override
+            public void onError(RESTException e)
+            {
+                MessageUtil.showError(e.getReason());
+            }
+        });
+        
+    }
+    
+    /**
+     * Show the successful message result of our REST call.
+     * 
+     * @param val    the value containing the JSON response from the server
+     */
+    private void showSuccessMessage(JSONValue val)
+    {
+        JSONObject obj = val.isObject();
+        String name = JSONUtil.getStringValue(obj, "user");
+        String browser = JSONUtil.getStringValue(obj, "userAgent");
+        String server = JSONUtil.getStringValue(obj, "serverInfo");
+        
+        String message = "Hello, " + name + "!  <br/>You are using " + browser + ".<br/>The server is " + server + ".";
+        m_longMessage.setHTML(message);
+    }
+}
