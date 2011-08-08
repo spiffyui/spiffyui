2f903dbe899ff2b76ce101b83d3e66ffe9528e84
zgrossbart@gmail.com <zgrossbart@gmail.com@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2011-08-08 18:20:26 +0000

    Added the Apache license header to the Maven plugin source files.

--- spiffyui/build/maven/maven-spiffyui-plugin/src/main/java/org/spiffyui/maven/plugins/ClosureMojo.java
+++ spiffyui/build/maven/maven-spiffyui-plugin/src/main/java/org/spiffyui/maven/plugins/ClosureMojo.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
+
 /* forked from https://github.com/gli/closure-compiler-maven-plugin/blob/34c2d4395db1b88e606e06bb19e9a6e6cdf9d9f0/src/main/java/glisoft/ClosureCompilerMojo.java */
 
 package org.spiffyui.maven.plugins;
