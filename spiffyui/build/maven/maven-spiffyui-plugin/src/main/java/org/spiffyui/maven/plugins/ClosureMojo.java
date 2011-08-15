2ea19c7038ff83d48a8ee9a6b9a7acb3e8bda895
zgrossbart@gmail.com <zgrossbart@gmail.com@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2011-08-15 13:40:38 +0000

    We are now setting the Closure Compiler logging level to WARNING so we don't see so many info messages when compiling JavaScript.

--- spiffyui/build/maven/maven-spiffyui-plugin/src/main/java/org/spiffyui/maven/plugins/ClosureMojo.java
+++ spiffyui/build/maven/maven-spiffyui-plugin/src/main/java/org/spiffyui/maven/plugins/ClosureMojo.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
@@ -125,6 +126,7 @@ public class ClosureMojo extends AbstractMojo
         }
 
         Compiler compiler = new Compiler();
+        compiler.setLoggingLevel(Level.WARNING);
         Result result = compiler.compile(new ArrayList<JSSourceFile>(), sources, compilerOptions);
 
         for (JSError warning : result.warnings) {
