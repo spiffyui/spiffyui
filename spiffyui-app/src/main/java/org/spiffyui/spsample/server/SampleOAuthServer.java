c036ec5f054470d67f78020ea5252fd8da48223c
Zack Grossbart <zack@grossbart.com>
2012-10-08 10:34:30 -0400

    Added a login form for the OAuth test harness.

--- spiffyui-app/src/main/java/org/spiffyui/spsample/server/SampleOAuthServer.java
+++ spiffyui-app/src/main/java/org/spiffyui/spsample/server/SampleOAuthServer.java
@@ -18,7 +18,15 @@
 package org.spiffyui.spsample.server;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,9 +38,33 @@ import javax.servlet.http.HttpServletResponse;
 public class SampleOAuthServer extends HttpServlet
 {
     private static final long serialVersionUID = -1L;
+
+    /** The contents of the login form */
+    private static String g_loginForm;
     
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        String redirectUri = request.getParameterValues("redirect_uri")[0];
+
+        String clientId = "null";
+
+        if (request.getParameterValues("clientId") != null) {
+            clientId = request.getParameterValues("clientId")[0];
+        }
+
+        String state = "null";
+
+        if (request.getParameterValues("state") != null) {
+            state = request.getParameterValues("state")[0];
+        }
+
+        loginForm(request, response, clientId, state, redirectUri);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException
     {
         StringBuffer hash = new StringBuffer();
@@ -43,14 +75,83 @@ public class SampleOAuthServer extends HttpServlet
         hash.append("&access_token=1234");
 
         String redirectUri = request.getParameterValues("redirect_uri")[0];
-        
+
         response.sendRedirect(redirectUri + hash.toString());
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException
+    private void loginForm(HttpServletRequest request, HttpServletResponse response, String clientID, String state, String redirectURI)
     {
-        doGet(request, response);
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("text/html");
+        PrintWriter out;
+        try {
+            out = response.getWriter();
+        } catch (IOException e) {
+            e.printStackTrace();
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+        out.println(loadLoginForm(request.getSession().getServletContext(), request.getLocale(), clientID, state, redirectURI));
+        out.close();
+        return;
+    }
+
+    private static String loadLoginForm(ServletContext context)
+    {
+        if (g_loginForm == null) {
+            InputStream in = context.getResourceAsStream("/login.html");
+            Scanner scanner =  new Scanner(in, "UTF-8");
+            g_loginForm = scanner.useDelimiter("\\A").next();
+        }
+
+        return g_loginForm;
+    }
+
+    /**
+     * Load the login form text and inject the strings with localized and
+     * dynamic values.
+     * 
+     * @param context  the servlet context to load the form with
+     * @param locale   the current locale
+     * @param clientID the client ID to add to the form
+     * @param state    the state to add to the form
+     * @param redirectURI
+     *                 the redirection URI once the user logs in
+     * 
+     * @return the localized form with all of the values injected.
+     */
+    private String loadLoginForm(ServletContext context, Locale locale, String clientID, String state, String redirectURI)
+    {
+        StringBuffer sb = new StringBuffer(loadLoginForm(context));
+        
+        Pattern p = Pattern.compile("\\$\\{.*\\}");
+        Matcher matcher = p.matcher(sb.toString());
+        int offset = 0;
+        
+        while (matcher.find()) {
+            String key = matcher.group().substring(2, matcher.group().length() - 1);
+            String val = null;
+
+            if ("clientID".equals(key)) {
+                val = clientID;
+            } else if ("state".equals(key)) {
+                val = state;
+            } else if ("redirectURI".equals(key)) {
+                val = redirectURI;
+            } else {
+                throw new IllegalArgumentException("They key " + key + " wasn't found in the login form");
+            }
+
+            sb.replace(matcher.start(), matcher.end(), val);
+
+            /*
+             * We have to search again since we just changed all of the offsets
+             * by replacing a string with a new string of a different length.
+             */
+            matcher = p.matcher(sb.toString());
+        }
+
+        return sb.toString();
+        
     }
 }
